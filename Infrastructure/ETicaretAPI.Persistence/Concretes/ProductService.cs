02979e0b5d6b1013867f4ecc461fd66b05f00f11
Arif Babayev <arifrb@code.edu.az>
2022-11-27 13:36:51 +0400

    Simple Api Structure Created

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+
+     public List<Product> GetProducts()
+         => new()
+             {
+               new()
+                 {
+                    Id = Guid.NewGuid(),
+                   Name="Product",
+                   Price=100,
+                   Stock=10
+                 },
+               new()
+                 {
+                    Id = Guid.NewGuid(),
+                   Name="Product2",
+                   Price=1100,
+                   Stock=1
+                 },
+               new()
+                 {
+                    Id = Guid.NewGuid(),
+                   Name="Product3",
+                   Price=2100,
+                   Stock=14
+                 },
+               new()
+                 {
+                    Id = Guid.NewGuid(),
+                   Name="Product4",
+                   Price=500,
+                   Stock=20
+                 }
+             };
+    }
+}
+
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ETicaretAPI.Application.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var product = _productService.GetProducts();
+            return Ok(product);
+        }
+
+      
+    }
+}
+
