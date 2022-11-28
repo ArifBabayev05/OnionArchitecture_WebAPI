9067c3c672033dc60edc3e8e10574a9c6e1207c7
Arif Babayev <arifrb@code.edu.az>
2022-11-28 23:51:54 +0400

    Start Migrations

---
+++ Core/ETicaretAPI.Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ETicaretAPI.Domain.Entities
+{
+	public class Order
+	{
+		public Order()
+		{
+		}
+	}
+}
+
---
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ETicaretAPI.Persistence.Contexts
+{
+	public class ETicaretDbContext
+	{
+		public ETicaretDbContext()
+		{
+		}
+	}
+}
+
